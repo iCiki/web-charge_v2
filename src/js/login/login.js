6e2500ee5474e9d3b3f284d83c2483d71b9b29e8
iCiki <917518045@qq.com>
2020-11-21 14:37:41 +0800

    finish home main structure page

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -1,5 +1,3 @@
-import 'weui.js'
-
 import tpl from 'raw!./login.html';
 
 
@@ -13,19 +11,59 @@ export default {
     },
     bind: function () {
         $("#sendmsg").on('click', function () {
+            var phone = isMobileNumber($("#phone").val());
+            if(!phone){
+               $.weui.topTips("请输入正确的手机号！");
+               return;
+            }
             sendmessage("#sendmsg");
         });
 
         $("#login_bt").on('click', function () {
+            if(!isMobileNumber($("#phone").val())){
+                $.weui.topTips("请输入正确手机号！");
+                return;
+            }
+            if(!checkVerifyCode($("#verify_code").val())){
+                $.weui.topTips("请输入正确4位验证码！", "forbidden");
+                return;
+            }
             console.log("login_bt clicked...");
-            var loading = $.weui.loading('loading');
+            $.weui.loading("登录中...");
             setTimeout(function () {
-                loading.hide();
+                $.weui.hideLoading();
             }, 3000);
         });
     }
 };
 
+function checkVerifyCode(verifyCode) {
+    return verifyCode.length === 4;
+}
+
+function isMobileNumber(phone) {
+    var flag = false;
+    var message = "";
+    var myreg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0-9]{1})|(15[0-3]{1})|(15[4-9]{1})|(18[0-9]{1})|(199))+\d{8})$/;
+    if (phone == '') {
+        // console.log("手机号码不能为空");
+        message = "手机号码不能为空！";
+    } else if (phone.length != 11) {
+        //console.log("请输入11位手机号码！");
+        message = "请输入11位手机号码！";
+    } else if (!myreg.test(phone)) {
+        //console.log("请输入有效的手机号码！");
+        message = "请输入有效的手机号码！";
+    } else {
+        flag = true;
+    }
+    if (message != "") {
+        // alert(message);
+    }
+    return flag;
+}
+
+var loading = null;
 var second = 60;
 function sendmessage(name) {
     console.log("...sendmessage clicked...");
@@ -40,7 +78,7 @@ function sendmessage(name) {
         }
         else {
             var printnr = second - num;
-            $(name).text(printnr + "秒后获取");
+            $(name).text(printnr + "秒后重新获取");
             $(name).attr("disabled", true);
         }
     }
